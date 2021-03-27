0447498aea15d03cf34dd95d1a88019591d9928b
Sergey-Kosichkin <d978827@icloud.com>
2021-03-27 07:47:40 +0300

    Added logic to the code.

--- customViewRGB/ViewController.swift
+++ customViewRGB/ViewController.swift
@@ -19,21 +19,36 @@ class ViewController: UIViewController {
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         rgbView.layer.cornerRadius = 20
-        
-        
     }
     
+   
     @IBAction func redSliderMoved() {
+        redLabel.text = String(format: "%.2f", redSlider.value)
+        changeViewColor()
     }
     
     @IBAction func greenSliderMoved() {
+        greenLabel.text = String(format: "%.2f", greenSlider.value)
+        changeViewColor()
     }
     
     @IBAction func blueSliderMoved() {
+        blueLabel.text = String(format: "%.2f", blueSlider.value)
+        changeViewColor()
+    }
+    
+}
+
+extension ViewController {
+    func changeViewColor() {
+        rgbView.backgroundColor = UIColor(red: CGFloat(redSlider.value),
+                                          green: CGFloat(greenSlider.value),
+                                          blue: CGFloat(blueSlider.value),
+                                          alpha: 1)
     }
 }
 
